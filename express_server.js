fe1d65ab615482cec2a504f3f4aa8155d643a341
Lauren <laurenribeiro06@gmail.com>
2019-02-12 23:20:07 +0000

    Added function to generate a random string of characters

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs")
 
@@ -9,6 +10,17 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+function generateRandomString() {
+  var randomString = "";
+  var possibleChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 7; i++) {
+    randomString += possibleChar.charAt(Math.floor(Math.random() * possibleChar.length))
+  }
+  return(randomString);
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,7 +43,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {shortURL : req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
