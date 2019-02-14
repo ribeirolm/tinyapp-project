64200155bafa63da4d7de4baeeeb29cc5b94f978
Lauren <laurenribeiro06@gmail.com>
2019-02-14 03:38:12 +0000

    Added users object to store and access users in the app

--- express_server.js
+++ express_server.js
@@ -12,6 +12,20 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+
 
 function generateRandomString() {
   var randomString = "";
